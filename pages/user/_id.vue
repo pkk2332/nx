27285d5fa3fb8a86eddc393e99bdb9cf190ac15f
pkk2332 <babyking67@gmail.com>
2018-12-26 01:24:25 +0630

    filtering

---
+++ pages/user/_id.vue
@@ -0,0 +1,40 @@
+<template>
+	
+<div>
+	<p>aaa-{{ xx.id }}</p>
+</div>
+
+</template>
+
+
+<script>
+	
+	export default {
+		data(){
+			return{
+				somedata:[],
+				id:null
+			}
+		},
+		async asyncData({$axios}){
+
+		  const ip = await $axios.$get('https://jsonplaceholder.typicode.com/users')
+		   return {somedata:ip}
+		},
+		computed:{
+			
+			xx(){
+				var a =this.id
+				var b= this.somedata.filter(function(x){
+						return x.id==a
+				})
+				return b[0]
+			}
+		},
+		created(){
+			this.id=this.$route.params.id
+			console.log(this.id)
+
+		}
+	}
+</script>
---
+++ pages/user/index.vue
@@ -0,0 +1,54 @@
+
+
+
+
+<template>
+	<div>
+	<loading/></loading>
+
+	<div v-for="x in somedata">
+		<p>id - {{ x.id }}</p>
+				<p> 	<nuxt-link :to="'/user/'+x.id">name - {{ x.name }}</nuxt-link></p>
+				<p>username - {{ x.username }}</p>
+				<p>email - {{ x.email }}</p>
+				<p>phone - {{ x.phone }}</p>
+		
+	</div>
+</div>
+</template>
+
+<script>
+
+
+import loading from "~/.nuxt/components/nuxt-loading"
+
+export default {
+	data(){
+		return{
+			somedata:null
+		}
+	},
+	mounted(){
+		console.log()
+	},
+	async asyncData({ $axios }) {
+
+  const ip = await $axios.$get('https://jsonplaceholder.typicode.com/users')
+   return {somedata:ip}
+},
+	components:{
+		loading
+	},
+
+	layout:'layout2',
+
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+</style>
